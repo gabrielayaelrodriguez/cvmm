12a1929442c27a1ba2a8edec86d5884fd7bb9ee4
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-07-06 15:22:41 -0300

    dashboards

---
+++ app/controllers/dashboard/dashboard_controller.rb
@@ -0,0 +1,13 @@
+class Dashboard::DashboardController < ApplicationController
+	before_action :authenticate_user!
+  def index
+
+  	if current_user.admin
+
+  	          render "admin"
+
+  	        else
+  	          render "user"
+  	      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  namespace :dashboard do
+    get 'dashboard/index'
+  end
+
   devise_for :users
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
+
+  namespace :dashboard do
+      root to: "dashboard#index"
+  end
 end
