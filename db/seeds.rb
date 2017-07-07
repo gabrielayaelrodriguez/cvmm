afbc712c912467985a0efb7906f55a447a14bd16
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-07-07 02:16:28 -0300

    asdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create! :name => 'admin', :admin => true, :email => 'admin@admin', :password => 'topsecret', :password_confirmation => 'topsecret'
