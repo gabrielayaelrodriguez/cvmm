f8b40ca09755a5efd5d53776638e0083764db987
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-07-07 01:44:35 -0300

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  namespace :dashboard do
-    get 'dashboard/index'
-  end
-
-  devise_for :users
-  get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 
-  namespace :dashboard do
-      root to: "dashboard#index"
-  end
+  devise_for :users
+  resources :dashboard, only: [:index]
+
+  get 'home/index'
+
+  root to: "home#index"
 end
