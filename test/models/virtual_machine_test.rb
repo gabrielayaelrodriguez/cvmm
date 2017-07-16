13c29f26d2d75f52d7a539ef0cd3d35e337f69e5
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-07-16 20:32:01 -0300

    empezando tests

--- test/models/virtual_machine_test.rb
+++ test/models/virtual_machine_test.rb
@@ -4,4 +4,19 @@ class VirtualMachineTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+    @user = User.create(:name => 'user', :email => 'asd@asd', :password => 'pass', :password_confirmation => 'pass')
+  end
+
+  test "should not create a machine without attributes" do
+	  vm = VirtualMachine.new()
+	  assert_not vm.save
+  end
+
+  test "should create a machine successfully" do
+	  vm = VirtualMachine.new(name: 'myVM', os: 'linux', user: @user, cores: 0, memory: 0)
+	  assert vm.save
+  end
+
 end
