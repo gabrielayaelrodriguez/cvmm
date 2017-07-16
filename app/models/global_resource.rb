0ddbaf6a3d20e84084a1d313e3fe25b3ae89d689
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-07-16 19:05:00 -0300

    validadores para global resource

--- app/models/global_resource.rb
+++ app/models/global_resource.rb
@@ -1,7 +1,31 @@
 class GlobalResource < ApplicationRecord
 
 	#validates_numericality_of :totalRAM, :only_integer => true, :greater_than_or_equal_to => 0
-	#validates :totalRAM, numericality: { greater_than_or_equal_to: 0 }
+	validate :inUseDiskSpace
+	validate :inUseRAM
+	validate :inUseCPUCores
+
+
+	def inUseDiskSpace
+		usedSpace = Disk.all.sum(:capacity)
+		if self.totalDiskSpace < usedSpace
+			errors.add(:totalDiskSpace, "Total Disk Space must be more or equal than #{usedSpace}")
+		end
+	end
+
+	def inUseRAM
+		usedRAM = VirtualMachine.all.sum(:memory)
+		if self.totalRAM < usedRAM
+			errors.add(:totalRAM, "Total RAM must be more or equal than #{usedRAM}")
+		end
+	end
+
+	def inUseCPUCores
+		usedCPU = VirtualMachine.all.sum(:cores)
+		if self.totalCPUCores < usedCPU
+			errors.add(:totalCPUCores, "Total CPU cores must be more or equal than #{usedCPU}")
+		end
+	end
 
 	def freeDiskSpace
 		self.totalDiskSpace - Disk.all.sum(:capacity)
