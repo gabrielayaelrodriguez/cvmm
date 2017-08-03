b0e4922a2740d2aaba8155e97df714b2117161b7
Gabriela Yael Rodriguez <gabrielayaelrodriguez@gmail.com>
2017-08-03 15:15:14 -0300

    siguiendo con traducciones, mensajes de error, discos, todavia faltaa

--- app/models/virtual_machine.rb
+++ app/models/virtual_machine.rb
@@ -4,6 +4,8 @@ class VirtualMachine < ApplicationRecord
 
   validates :name, :state, :os, presence: true
 
+  POSSIBLE_STATES = ["shut_down", "sleeping", "running"]
+
   validates :memory, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :cores, numericality: { greater_than_or_equal_to: 0  }, presence: true
 
@@ -15,13 +17,13 @@ class VirtualMachine < ApplicationRecord
   def check_memory
     available = GlobalResource.first.freeRAM
     if VirtualMachine.exists?(id)then available=available+VirtualMachine.find(id).memory end
-    errors.add(:memory, "Memory must be less or equal than #{available}") if memory && memory > available
+    errors.add(:memory, I18n.t("virtual_machine_errors.memory_less_or_equal_to", :available=> available)) if memory && memory > available
   end
 
   def check_cores
     available = GlobalResource.first.freeCPUCores
     if VirtualMachine.exists?(id)then available=available+VirtualMachine.find(id).cores end
-    errors.add(:cores, "Cores must be less or equal than #{available}") if cores && cores > available
+    errors.add(:cores, I18n.t("virtual_machine_errors.cores_less_or_equal_to", :available=> available)) if cores && cores > available
   end
 
 end
